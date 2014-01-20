1e10ee83975df5fe8d3c11f126656aeaccb224b4
michael <michael.monticolo@gmail.com>
2014-01-20 19:53:28 +0100

    major bug fix - addon working as intended

--- _FEENIX_BG_FIX.lua
+++ _FEENIX_BG_FIX.lua
@@ -2,8 +2,8 @@ local flf = CreateFrame("frame",nil, UIParent);
 local Original_CancelLogout = CancelLogout
 local Original_Logout = Logout
 local OriginalStaticPopup_OnShow = StaticPopup_OnShow
-local active_logout = nil
 
+local player_logout = nil
 local timer_death = nil
 local timer_logout = nil
 
@@ -18,59 +18,29 @@ FLF_Event = function()
 	StaticPopup_OnShow = FixStaticPopup_OnShow
 end
 
-FLF_Update = function()
-	local time = GetTime()
-	
-	if timer_death and time > timer_death then
-		timer_death = nil
-		StaticPopup_Hide("DEATH");
-		
-	elseif timer_logout and time > timer_logout then
-		StaticPopup_Hide("CAMP");	
-		timer_logout = nil
-	end 
-end
-
 flf:SetScript("OnEvent",FLF_Event);
-flf:SetScript("OnUpdate",FLF_Update);
-
-
-FixStaticPopup_OnShow = function()
-	if this.which == "CAMP" then --and (GetRealZoneText() == "Alterac Valley" or GetRealZoneText() == "Arathi Basin" or GetRealZoneText() == "Warsong Gulch") and not active_logout then
-		DEFAULT_CHAT_FRAME:AddMessage(this.which)
-		if not active_logout then
-			DEFAULT_CHAT_FRAME:AddMessage("_BG Fix - Fake Logout Detected --------------------------------")
-			--this:Hide()
-			--StaticPopup_Hide("CAMP");
-			timer_death = GetTime() + 0.25
-			
-		end	
-	elseif this.which == "DEATH" and UnitHealth("player") > 0 then
-		DEFAULT_CHAT_FRAME:AddMessage("_BG Fix - Fake Release Spirit Detected ---------------------------")
-		--this:Hide()
-		--StaticPopup_Hide("DEATH"); 
-		timer_logout = GetTime() + 0.25
-	else
-		OriginalStaticPopup_OnShow()
-	end
-end
-
-ClosePopUp = function(popup)
-	for i=1,STATICPOPUP_NUMDIALOGS do
-		local frame = getglobal("StaticPopup"..i)
-		if frame:IsShown() and frame.which == popup then
-			frame:Hide()
-		end
-	end
-end
 
 Fix_Logout = function()
-	active_logout = true
+	player_logout = true
 	Original_Logout()
 end
 
 Fix_CancelLogout = function()
-	active_logout = nil
+	player_logout = nil
 	Original_CancelLogout()
-	ClosePopUp("CAMP")
+	StaticPopup_Hide("CAMP");
+end
+
+FixStaticPopup_OnShow = function()
+	if this.which == "CAMP" then
+		if not player_logout then
+			--DEFAULT_CHAT_FRAME:AddMessage("_BG Fix - Fake Logout Detected")
+			StaticPopup_Hide("CAMP");
+		end	
+	elseif this.which == "DEATH" and UnitHealth("player") > 0 then
+		--DEFAULT_CHAT_FRAME:AddMessage("_BG Fix - Fake Release Spirit Detected")
+		StaticPopup_Hide("DEATH");	
+	end
+	
+	OriginalStaticPopup_OnShow()
 end
