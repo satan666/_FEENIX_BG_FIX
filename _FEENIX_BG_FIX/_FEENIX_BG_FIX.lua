9edf0d7563feb258c2c41b8d32885c0e923cac9a
Michael <michael.monticolo@gmail.com>
2014-07-05 18:46:43 +0200

    exit popup fix

--- _FEENIX_BG_FIX/_FEENIX_BG_FIX.lua
+++ _FEENIX_BG_FIX/_FEENIX_BG_FIX.lua
@@ -1,6 +1,7 @@
 local flf = CreateFrame("frame",nil, UIParent); 
 local Original_CancelLogout = CancelLogout
 local Original_Logout = Logout
+local Original_Quit = Quit
 local OriginalStaticPopup_OnShow = StaticPopup_OnShow
 
 local player_logout = nil
@@ -10,29 +11,45 @@ local timer_logout = nil
 flf:RegisterEvent("ADDON_LOADED");
 
 FLF_Event = function()
-	DEFAULT_CHAT_FRAME:AddMessage("_FEENIX_BG_FIX by Ogrisch loaded")
+	--DEFAULT_CHAT_FRAME:AddMessage("_FEENIX_BG_FIX by Ogrisch loaded")
 	flf:UnregisterEvent("ADDON_LOADED");
 	
 	Logout = Fix_Logout
+	Quit = Fix_Quit
 	CancelLogout = Fix_CancelLogout
 	StaticPopup_OnShow = FixStaticPopup_OnShow
+	
 end
 
 flf:SetScript("OnEvent",FLF_Event);
 
+Fix_Quit = function()
+	--DEFAULT_CHAT_FRAME:AddMessage("Fix_Quit")
+	player_logout = true
+	Original_Quit()
+end
+
 Fix_Logout = function()
+	--DEFAULT_CHAT_FRAME:AddMessage("Fix_Logout")
 	player_logout = true
 	Original_Logout()
 end
 
 Fix_CancelLogout = function()
+	--DEFAULT_CHAT_FRAME:AddMessage("Fix_CancelLogout")
 	player_logout = nil
 	Original_CancelLogout()
 	StaticPopup_Hide("CAMP");
 end
 
 FixStaticPopup_OnShow = function()
-	if this.which == "CAMP" then
+	--DEFAULT_CHAT_FRAME:AddMessage(this.which)
+	if this.which == "QUIT" then
+		if not player_logout then
+			--DEFAULT_CHAT_FRAME:AddMessage("_BG Fix - Fake Quit Detected")
+			StaticPopup_Hide("QUIT");
+		end	
+	elseif this.which == "CAMP" then
 		if not player_logout then
 			--DEFAULT_CHAT_FRAME:AddMessage("_BG Fix - Fake Logout Detected")
 			StaticPopup_Hide("CAMP");
